38358954ff228df20def5d91a2d67dbd8ee47eec
Paul <paulgirmes@hotmail.com>
2020-09-12 17:57:06 +0200

    set

--- settings/production_settings.py
+++ settings/production_settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-import json
+
 import os
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
@@ -140,22 +140,20 @@ GS_PROJECT_ID = "p11oc-283117"
 
 MEDIA_URL = 'https://storage.cloud.google.com/{}/'.format(GS_BUCKET_NAME)
 MEDIA_ROOT = "media/"
-credentials = {
-  "type": "service_account",
-  "project_id": os.environ.get("project_id"),
-  "private_key_id": os.environ.get("private_key_id"),
-  "private_key": os.environ.get("private_key"),
-  "client_email": "p11oc-283117@appspot.gserviceaccount.com",
-  "client_id": os.environ.get("client_id"),
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/p11oc-283117%40appspot.gserviceaccount.com"
-}
-with open("google-credentials.json", "w") as fd:
-    json.dump(credentials, fd)
-GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
-    os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
+
+GS_CREDENTIALS = service_account.Credentials.from_service_account_info(
+    {
+        "type": "service_account",
+        "project_id": os.environ.get("project_id"),
+        "private_key_id": os.environ.get("private_key_id"),
+        "private_key": os.environ.get("private_key"),
+        "client_email": "p11oc-283117@appspot.gserviceaccount.com",
+        "client_id": os.environ.get("client_id"),
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/p11oc-283117%40appspot.gserviceaccount.com"
+    }
 )
 
 # debug toolbar specific setting
